b8f83b7d99bdc659536ccc4eba399049c5afc239
Moyeen Avaram <moyeen.avaram@sap.com>
2019-03-20 14:30:45 -0700

    conflict readme

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,11 +1,28 @@
 import express = require('express');
-
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- server/build/app.js
+++ server/build/app.js
@@ -1,11 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
+var graphqlHTTP = require('express-graphql');
+var buildSchema = require('graphql').buildSchema;
 // Create a new express application instance
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema("\n  type Query {\n    hello: String\n  }\n");
+// The root provides a resolver function for each API endpoint
+var root = {
+    hello: function () {
+        return 'Hello world!';
+    },
+};
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
