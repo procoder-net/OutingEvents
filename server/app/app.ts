9cb6ca107386d2f2a3bd07a89b32af2f05c9950e
Shravan Canchi Radhakrishna <shravan.canchi.radhakrishna@sap.com>
2019-03-20 18:14:59 -0700

    connected to postgres

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,6 +1,9 @@
 import express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
+var pg = require('pg');
+var client = new pg.Client();
+
 // Create a new express application instance
 const app: express.Application = express();
 
@@ -23,6 +26,36 @@ app.use('/graphql', graphqlHTTP({
     rootValue: root,
     graphiql: true,
 }));
+
+app.get('/api', (req, res) => {
+    client.connect()
+        .then(() => {
+            const query:string = 'SELECT * FROM example_table;'
+            const params:string[] = [];
+            return client.query(query, params);
+        })
+        .then((data:any) => {
+            res.send(data);
+        })
+        .catch((err:any) => {
+            console.log(err);
+        })
+});
+
+app.post('/api', (req, res) => {
+    client.connect()
+        .then(() => {
+            const query:string = "INSERT INTO example_table VALUES('person2');";
+            const params:string[] = [];
+            return client.query(query, params);
+        })
+        .then((data:any) => {
+            res.send(data);
+        })
+        .catch((err:any) => {
+            console.log(err);
+        })
+});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
--- server/build/app.js
+++ server/build/app.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var graphqlHTTP = require('express-graphql');
 var buildSchema = require('graphql').buildSchema;
+var pg = require('pg');
+var client = new pg.Client();
 // Create a new express application instance
 var app = express();
 // Construct a schema, using GraphQL schema language
@@ -18,6 +20,34 @@ app.use('/graphql', graphqlHTTP({
     rootValue: root,
     graphiql: true,
 }));
+app.get('/api', function (req, res) {
+    client.connect()
+        .then(function () {
+        var query = 'SELECT * FROM example_table;';
+        var params = [];
+        return client.query(query, params);
+    })
+        .then(function (data) {
+        res.send(data);
+    })
+        .catch(function (err) {
+        console.log(err);
+    });
+});
+app.post('/api', function (req, res) {
+    client.connect()
+        .then(function () {
+        var query = "INSERT INTO example_table VALUES('person2');";
+        var params = [];
+        return client.query(query, params);
+    })
+        .then(function (data) {
+        res.send(data);
+    })
+        .catch(function (err) {
+        console.log(err);
+    });
+});
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
