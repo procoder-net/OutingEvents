37e5a8670c4d6931d615d5c9e003f53cda83ec5e
Shravan Canchi Radhakrishna <shravan.canchi.radhakrishna@sap.com>
2019-03-22 18:51:37 -0700

    added db tables

---
+++ sql/createtables.sql
@@ -0,0 +1,56 @@
+create schema if not exists 'outingevents';
+set schema 'outingevents';
+
+CREATE TABLE IF NOT EXISTS Event(
+	id integer NOT NULL PRIMARY KEY,
+	type varchar NOT NULL,
+	name varchar NOT NULL UNIQUE,
+	location varchar NOT NULL,
+	state varchar NOT NULL,
+	survey_id integer,
+	start_time timestamp NOT NULL,
+	end_time timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS SurveyQuestions (
+	id integer NOT NULL PRIMARY KEY,
+	name varchar,
+	event_id integer,
+	questions json
+);
+
+CREATE TABLE IF NOT EXISTS SurveyResults (
+	id integer NOT NULL PRIMARY KEY,
+	survey_question_id integer NOT NULL,
+	user_id integer NOT NULL,
+	event_id integer NOT NULL,
+	response json
+);
+
+CREATE TABLE IF NOT EXISTS CurrentUser (
+	id integer NOT NULL PRIMARY KEY,
+	first_name varchar NOT NULL,
+	last_name varchar NOT NULL,
+	email varchar NOT NULL,
+	username varchar NOT NULL UNIQUE,
+	password varchar NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Payment (
+	id integer NOT NULL PRIMARY KEY,
+	event_id integer NOT NULL,
+	user_id integer NOT NULL,
+	status varchar,
+	description varchar NOT NULL,
+	amount integer NOT NULL,
+	currency varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Receipt (
+	id integer NOT NULL PRIMARY KEY,
+	event_id integer NOT NULL,
+	vendor varchar,
+	description varchar NOT NULL,
+	amount integer NOT NULL,
+	currency varchar NOT NULL
+);
