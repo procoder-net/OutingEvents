8f0ac935fc2d35118a882a8bcb2d7dbd1d73c439
Shravan Canchi Radhakrishna <shravan.canchi.radhakrishna@sap.com>
2019-03-26 00:13:14 -0700

    added api for surveyquestions

--- server/app/app.ts
+++ server/app/app.ts
@@ -1,7 +1,6 @@
 import "reflect-metadata";
 const express = require("express");
 require("dotenv").config({ path: "variables.env" });
-const bodyParser = require("body-parser");
 var pg = require("pg");
 const client = new pg.Client();
 const cors = require("cors");
@@ -9,20 +8,24 @@ const cors = require("cors");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs } = require("./schema");
 const { resolvers } = require("./resolvers");
+const bodyParser = require("body-parser");
+
 //const { Event } = require("./entity/Event");
 import Event from "./entity/Event";
 
 import { createConnection, ConnectionOptions } from "typeorm";
+import SurveyQuestion from "./entity/SurveyQuestion";
+import SurveyResult from "./entity/SurveyResult";
 
 const ormConfig: ConnectionOptions = {
   type: "postgres",
-  host: process.env.PGHOST,
+  host: "localhost",
   port: 5432,
   username: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  entities: [Event],
-  synchronize: false,
+  entities: [Event, SurveyQuestion, SurveyResult],
+  synchronize: true,
   logging: false
 };
 console.log(JSON.stringify(ormConfig, null, 4));
@@ -42,6 +45,7 @@ const corsOptions = {
 createConnection(ormConfig)
   .then(async connection => {
     app.use(cors());
+    app.use(bodyParser.json());
     server.applyMiddleware({ app });
     const PORT = process.env.PORT || 3000;
     server.applyMiddleware({ app });
@@ -96,17 +100,18 @@ createConnection(ormConfig)
         });
     });
 
-    app.post("/event", (req: any, res: any) => {
-      const event = new Event();
-      event.name = "April fools";
-      event.type = "Jokes";
-      event.location = "Planet earth";
-      event.state = "planning";
-      event.survey_id = 367;
-      event.start_time = new Date(2019, 3, 1, 0, 0, 0, 0);
-      event.end_time = new Date(2019, 3, 2, 0, 0, 0, 0);
+    app.post("/api/event/:eventId/surveyQuestion", (req: any, res: any) => {
+      const body = req.params;
+      const surveyQuestion = new SurveyQuestion();
+      surveyQuestion.event_id = req.params.eventId;
+      console.log(req.body);
+      surveyQuestion.name = req.body.name;
+      surveyQuestion.questions = JSON.stringify({
+        question: req.body.name,
+        options: req.body.options
+      });
       connection.manager
-        .save(event)
+        .save(surveyQuestion)
         .then((result: any) => {
           res.status(202);
           res.send(result);
@@ -117,6 +122,46 @@ createConnection(ormConfig)
           console.log(JSON.stringify(err, null, 4));
         });
     });
+
+    app.get("/api/event/:eventId/surveyQuestion", (req: any, res: any) => {
+      connection.manager
+        .getRepository(SurveyQuestion)
+        .find({ event_id: req.params.eventId })
+        .then(surveyQuestions => {
+          surveyQuestions.forEach(function(obj) {
+            let questionString: string = obj.questions;
+            console.log(obj);
+            obj.questions = JSON.parse(questionString);
+          });
+          res.status(200);
+          res.send(surveyQuestions);
+        })
+        .catch(err => {
+          res.status(500);
+          res.send(err);
+        });
+    });
+
+    app.delete(
+      "/api/event/:eventId/surveyQuestion/:questionId",
+      (req: any, res: any) => {
+        connection.manager
+          .getRepository(SurveyQuestion)
+          .delete(req.params.questionId)
+          .then(surveyQuestion => {
+            res.status(200);
+            res.send({
+              status: "success",
+              id: req.params.questionId,
+              event_id: req.params.eventId
+            });
+          })
+          .catch(err => {
+            res.status(500);
+            res.send(err);
+          });
+      }
+    );
 
     app.listen({ port: PORT }, () =>
       console.log(
--- sql/droptables.sql
+++ sql/droptables.sql
@@ -1,7 +1,7 @@
 set schema 'outingevents';
 drop table if exists Event;
-drop table if exists SurveyQuestions;
-drop table if exists SurveyResults;
+drop table if exists SurveyQuestion;
+drop table if exists SurveyResult;
 drop table if exists CurrentUser;
 drop table if exists Payment;
 drop table if exists Receipt;
