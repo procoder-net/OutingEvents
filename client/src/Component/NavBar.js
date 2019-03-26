4f3f033ca669e409f848b18722e3ce834833719c
Moyeen Avaram <moyeen.avaram@sap.com>
2019-03-26 00:40:18 -0700

    some more navbar

--- client/src/Component/NavBar.js
+++ client/src/Component/NavBar.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react';
-import {
-    AppBar,
-    Toolbar,
-    Typography,
-    Link,
-    Button
-} from "@material-ui/core";
-import { withAuth } from '@okta/okta-react';
-import { compose } from 'redux'
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { AppBar, Toolbar, Typography, Link, Button } from "@material-ui/core";
+import { withAuth } from "@okta/okta-react";
+import { compose } from "redux";
+import styled from "styled-components";
 
 const StyledButton = styled(Button)`
   background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
@@ -17,63 +11,63 @@ const StyledButton = styled(Button)`
   color: white;
   height: 48px;
   padding: 0 30px;
-  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .3);
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
 `;
-
 
 let classes;
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { authenticated: null };
+    this.checkAuthentication = this.checkAuthentication.bind(this);
+    this.checkAuthentication();
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = { authenticated: null };
-        this.checkAuthentication = this.checkAuthentication.bind(this);
-        this.checkAuthentication();
-        this.login = this.login.bind(this);
-        this.logout = this.logout.bind(this);
+  async checkAuthentication() {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
     }
+  }
+  componentDidUpdate() {
+    this.checkAuthentication();
+  }
 
-    async checkAuthentication() {
-        const authenticated = await this.props.auth.isAuthenticated();
-        if (authenticated !== this.state.authenticated) {
-            this.setState({ authenticated });
-        }
-    }
-    componentDidUpdate() {
-        this.checkAuthentication();
-    }
+  async login() {
+    // Redirect to '/' after login
+    this.props.auth.login("/");
+  }
 
-    async login() {
-        // Redirect to '/' after login
-        this.props.auth.login('/');
-    }
+  async logout() {
+    // Redirect to '/' after logout
+    this.props.auth.logout("/");
+  }
 
-    async logout() {
-        // Redirect to '/' after logout
-        this.props.auth.logout('/');
-    }
-
-    render() {
-        const isLoggedIn = this.state.authenticated;
-        return (
-            <div>
-                <AppBar position="static">
-                    <Toolbar>
-                        <Typography variant="title" color="inherit" style={{ flex: 1 }}>
-                            Orgo Events
-                        </Typography>
-                        {isLoggedIn ? (
-                            <StyledButton onClick={this.logout} color="inherit" >Logout</StyledButton>
-                        ) : (
-                                <StyledButton onClick={this.login} color="inherit">Login</StyledButton>
-                            )}
-                    </Toolbar>
-                </AppBar>
-            </div>
-        );
-    }
+  render() {
+    const isLoggedIn = this.state.authenticated;
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit" style={{ flex: 1 }}>
+              Orgo Events
+            </Typography>
+            {isLoggedIn ? (
+              <StyledButton onClick={this.logout} color="inherit">
+                Logout
+              </StyledButton>
+            ) : (
+              <StyledButton onClick={this.login} color="inherit">
+                Login
+              </StyledButton>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
 }
 
-export default compose(
-    withAuth
-)(NavBar);
+export default compose(withAuth)(NavBar);
