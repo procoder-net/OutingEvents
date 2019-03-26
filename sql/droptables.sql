fc8c18637a899cfd31c2f2e93e1cb7dd53330c97
Shravan Canchi Radhakrishna <shravan.canchi.radhakrishna@sap.com>
2019-03-26 00:39:53 -0700

    forgot to change drop table script

--- sql/droptables.sql
+++ sql/droptables.sql
@@ -1,7 +1,7 @@
 set schema 'outingevents';
 drop table if exists Event;
-drop table if exists SurveyQuestion;
-drop table if exists SurveyResult;
+drop table if exists Survey_Question;
+drop table if exists Survey_Result;
 drop table if exists CurrentUser;
 drop table if exists Payment;
 drop table if exists Receipt;
