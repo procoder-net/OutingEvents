3b588ed9ce45d1880a9f7c7c362b8b8012f8809b
Moyeen Avaram <moyeen.avaram@sap.com>
2019-03-26 00:39:03 -0700

    Adding Login via OKTA to ORGO

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,48 @@ import React from "react";
 import NavBar from "./Component/NavBar";
 import SurveyDisplay from "./Component/Survey";
 import HomePage from "./homePage";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Home from "./Home";
+import Login from "./Component/auth/Login";
 import PaymentRec from "./Component/PaymentRec";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
 import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
+const config = {
+  issuer: "https://dev-904688.okta.com/oauth2/default",
+  redirect_uri: window.location.origin + "/implicit/callback",
+  client_id: "0oady6mi7hzP6gHUF356"
+};
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <NavBar />
-          <h1>Home</h1>
-        </div>
-        <div className="content">
-          <Route exact path="/" component={HomePage} />
-          <Route path="/survey" component={SurveyDisplay} />
-          <Route path="/paymentRec" component={PaymentRec} />
-        </div>
+        <Security
+          issuer={config.issuer}
+          client_id={config.client_id}
+          redirect_uri={config.redirect_uri}
+          onAuthRequired={onAuthRequired}
+        >
+          <CssBaseline />
+          <div className="App">
+            <NavBar />
+          </div>
+          <div className="content">
+            <SecureRoute path="/" exact component={HomePage} />
+            <Route
+              path="/login"
+              render={() => <Login baseUrl="https://dev-904688.okta.com" />}
+            />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+            <SecureRoute exact path="/events" component={HomePage} />
+            <SecureRoute path="/survey" component={SurveyDisplay} />
+            <SecureRoute path="/paymentRec" component={PaymentRec} />
+          </div>
+        </Security>
       </BrowserRouter>
     );
   }
