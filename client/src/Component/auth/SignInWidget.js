7f8137b96452214f647e4cb156f2b7bcec360eba
Moyeen Avaram <moyeen.avaram@sap.com>
2019-03-28 11:16:28 -0700

    enable signup and logo

--- client/src/Component/auth/SignInWidget.js
+++ client/src/Component/auth/SignInWidget.js
@@ -5,13 +5,18 @@ import ReactDOM from "react-dom";
 import OktaSignIn from "@okta/okta-signin-widget";
 import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 import "@okta/okta-signin-widget/dist/css/okta-theme.css";
+import logo from "../../../public/images/orgo2.png";
 
 export default class OktaSignInWidget extends Component {
   componentDidMount() {
     const el = ReactDOM.findDOMNode(this);
     this.widget = new OktaSignIn({
       baseUrl: this.props.baseUrl,
-      logo: "/images/orgo.png"
+      logo: logo,
+      features: {
+        registration: true, // Enable self-service registration flow
+        rememberMe: true
+      }
     });
     this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError);
   }
