bb46a21299bbd8e4de6e36995decfee30f30c612
i822370 <kyrylo.kochubyey@sap.com>
2019-03-28 14:23:53 -0700

    Adding details markdown test text

--- client/src/Component/EventList.js
+++ client/src/Component/EventList.js
@@ -6,7 +6,14 @@ const event = {
   image:
     "https://93546-d-c.ooyala.com/content/images/1208/1542184503_3805a158-e563-4eba-a31b-105cf488feaa_636x357.jpg",
   title: "Ski Event",
-  description: "Ski Trip"
+  description: "Ski Trip",
+  details:
+    "## Ski vent detail \
+            \
+            #### April 1, 2020 by [Mike](/) \
+            \
+            Lets go ski ... \
+        "
 };
 const EventList = () => {
   return (
