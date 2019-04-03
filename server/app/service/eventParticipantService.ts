efbb7303d4cc123a1e14a545c454c9a3bd92466c
Moyeen Avaram <moyeen.avaram@sap.com>
2019-04-03 04:10:49 -0700

    merging left out

--- server/app/service/eventParticipantService.ts
+++ server/app/service/eventParticipantService.ts
@@ -3,85 +3,90 @@ import Event from "../entity/Event";
 
 import connectORM from "./../connection";
 import { getEventByEventId } from "./eventService";
-const populate = ['event', 'survey_result', 'payments'];
+const populate = ["event", "survey_result", "payments"];
 export async function addEventParticipant(
-    user: string,
-    event_id: number,
-    isOrganizer: boolean,
-    confirmed: boolean,
-    attended: boolean
+  user: string,
+  event_id: number,
+  isOrganizer: boolean,
+  confirmed: boolean,
+  attended: boolean
 ) {
-    const eventParticipant = new EventParticipant();
-    //eventParticipant.user_id = user_id;
-    eventParticipant.is_organizer = isOrganizer;
-    //when participant is created, all of these are false
-    eventParticipant.attended = attended;
-    eventParticipant.confirmed = confirmed;
-    eventParticipant.user = user;
-    eventParticipant.event = await getEventByEventId(event_id);
-    return await connectORM.getRepository(EventParticipant).save(eventParticipant);
+  const eventParticipant = new EventParticipant();
+  //eventParticipant.user_id = user_id;
+  eventParticipant.is_organizer = isOrganizer;
+  //when participant is created, all of these are false
+  eventParticipant.attended = attended;
+  eventParticipant.confirmed = confirmed;
+  eventParticipant.user = user;
+  eventParticipant.event = await getEventByEventId(event_id);
+  return await connectORM
+    .getRepository(EventParticipant)
+    .save(eventParticipant);
 }
 
 export function removeEventParticipant(participant_id: number) {
-    return connectORM
-        .getRepository(EventParticipant)
-        .delete(participant_id)
-        .then((result: any) => {
-            return result;
-        })
-        .catch(err => {
-            throw err;
-        });
+  return connectORM
+    .getRepository(EventParticipant)
+    .delete(participant_id)
+    .then((result: any) => {
+      return result;
+    })
+    .catch(err => {
+      throw err;
+    });
 }
 
-export function getEventParticipants(event_id: number, id?: number, populateRelations = true): any {
-    let find: any = {
-        event: event_id
-    };
-    if (id) {
-        find.id = id;
-    }
-    if (populateRelations) {
-        find.relations = populate;
-    }
-    return connectORM
-        .getRepository(EventParticipant)
-        .find(find)
-        .then(eventParticipants => {
-            return eventParticipants;
-        })
-        .catch(err => {
-            throw err;
-        });
+export function getEventParticipants(
+  event_id: number,
+  id?: number,
+  populateRelations = true
+): any {
+  let find: any = {
+    event: event_id
+  };
+  if (id) {
+    find.id = id;
+  }
+  if (populateRelations) {
+    find.relations = populate;
+  }
+  return connectORM
+    .getRepository(EventParticipant)
+    .find(find)
+    .then(eventParticipants => {
+      return eventParticipants;
+    })
+    .catch(err => {
+      throw err;
+    });
 }
 
 export function updateEventParticipantStatus(
-    participant_id: number,
-    updatedData: any
+  participant_id: number,
+  updatedData: any
 ) {
-    return connectORM
-        .getRepository(EventParticipant)
-        .findOne({ id: participant_id })
-        .then((eventParticipant: any) => {
-            eventParticipant.notified = updatedData.notified
-                ? updatedData.notified
-                : eventParticipant.notified;
-            eventParticipant.confirmed = updatedData.confirmed
-                ? updatedData.confirmed
-                : eventParticipant.confirmed;
-            eventParticipant.attended = updatedData.attended
-                ? updatedData.attended
-                : eventParticipant.attended;
-            eventParticipant.tooksurvey = updatedData.tooksurvey
-                ? updatedData.tooksurvey
-                : eventParticipant.tooksurvey;
-            return connectORM.getRepository(EventParticipant).save(eventParticipant);
-        })
-        .catch(err => {
-            throw err;
-        });
+  return connectORM
+    .getRepository(EventParticipant)
+    .findOne({ id: participant_id })
+    .then((eventParticipant: any) => {
+      eventParticipant.notified = updatedData.notified
+        ? updatedData.notified
+        : eventParticipant.notified;
+      eventParticipant.confirmed = updatedData.confirmed
+        ? updatedData.confirmed
+        : eventParticipant.confirmed;
+      eventParticipant.attended = updatedData.attended
+        ? updatedData.attended
+        : eventParticipant.attended;
+      eventParticipant.tooksurvey = updatedData.tooksurvey
+        ? updatedData.tooksurvey
+        : eventParticipant.tooksurvey;
+      return connectORM.getRepository(EventParticipant).save(eventParticipant);
+    })
+    .catch(err => {
+      throw err;
+    });
 }
-
 
 // tested get and add
 // need to test update and delete
