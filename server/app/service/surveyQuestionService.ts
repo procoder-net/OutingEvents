6cb78cad6b858a14df03e5efd392ed696bead9ef
Arturo Duarte <arturo.duarte@sap.com>
2019-04-04 09:22:23 -0700

    Added service for Counted Results
    Added service for Counted Results

--- server/app/service/surveyQuestionService.ts
+++ server/app/service/surveyQuestionService.ts
@@ -154,5 +154,123 @@ export function getSurveyResultsByEvent(
     });
 }
 
+export function getCountedSurveyResultsByEvent(event_id: number) {
+  var surveyRecord: any;
+
+  class Question {
+    question: string;
+    answers: string[] = [];
+    counts: number[] = [];
+  }
+
+  class newFormQuestion {
+    question: string;
+    answers: any[] = [];
+  }
+
+  return connectORM
+    .getRepository(SurveyResult)
+    .find({ event: event_id })
+    .then(eventSurveyResults => {
+      var surveyNo: number = 0;
+      var questionNo: number = 0;
+      var questionKeys: string[];
+      var answerValues: string[];
+      var questions: Question[] = [];
+      var countarray: number[] = [];
+      var firstpass: number = 0;
+      var answerIndex: number;
+      var newQuestion: boolean;
+      for (let surveyRecord_i of eventSurveyResults) {
+        //console.log("SurveyResult : " + surveyNo);
+        surveyRecord = surveyRecord_i;
+        questionKeys = Object.keys(surveyRecord.response);
+        Object.keys(surveyRecord.response).forEach(key => {
+          answerValues = surveyRecord.response[key];
+          //console.log(questionKeys[questionNo]);
+          //console.log(answerValues);
+          if (firstpass == 0) {
+            let question_i = new Question();
+            question_i.question = questionKeys[questionNo];
+            question_i.answers = answerValues;
+            for (let answer_j in answerValues) {
+              countarray.push(1);
+            }
+            question_i.counts = countarray;
+            countarray = [];
+            questions.push(question_i);
+          } else {
+            newQuestion = true;
+            for (let j in questions) {
+              let question_j = new Question();
+              question_j = questions[j];
+              if (questionKeys[questionNo] == question_j.question) {
+                for (let answer of answerValues) {
+                  console.log(answer);
+                  answerIndex = question_j.answers.indexOf(answer);
+                  if (answerIndex > -1) {
+                    question_j.counts[answerIndex] += 1;
+                    questions[j].counts = question_j.counts;
+                  } else {
+                    question_j.answers.push(answer);
+                    question_j.counts.push(1);
+                    questions[j].answers = question_j.answers;
+                    questions[j].counts = question_j.counts;
+                  }
+                }
+                newQuestion = false;
+                break;
+              }
+            }
+            if (newQuestion) {
+              let question_j = new Question();
+              question_j.question = questionKeys[questionNo];
+              question_j.answers = answerValues;
+              for (let answer_k in answerValues) {
+                countarray.push(1);
+              }
+              question_j.counts = countarray;
+              countarray = [];
+              questions.push(question_j);
+            }
+          }
+
+          questionNo += 1;
+        });
+
+        questionNo = 0;
+        surveyNo += 1;
+        firstpass = 1;
+      }
+
+      //console.log("here are the question counts");
+      //console.log(questions);
+
+      // Change the return format
+
+      var newFormuestions: newFormQuestion[] = [];
+
+      for (let i in questions) {
+        let question_i = new newFormQuestion();
+        question_i.question = questions[i].question;
+        for (let answer_j in questions[i].answers) {
+          question_i.answers.push({
+            answer: questions[i].answers[answer_j],
+            count: questions[i].counts[answer_j]
+          });
+        }
+        newFormuestions.push(question_i);
+      }
+
+      console.log(newFormuestions);
+
+      return newFormuestions;
+    })
+    .catch(err => {
+      console.log("No results found for event: " + event_id);
+      throw err;
+    });
+}
+
 // tested get and add
 // need to test update and delete
