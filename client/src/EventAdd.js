62de0bb370a75c05a18c71b4b7f70a31624b189e
Mohammed Moyeen Avaram <procoder.net@gmail.com>
2019-04-04 11:20:28 -0700

    fixing survey and adding call to Arturo to debug (#38)

--- client/src/EventAdd.js
+++ client/src/EventAdd.js
@@ -27,7 +27,7 @@ class EventAdd extends React.Component {
           hour: parseInt(deadlineTime[0]),
           minute: parseInt(deadlineTime[1])
         },
-        survey: 5,
+        survey: 1,
         location: "san jose",
         invited: input.invited.map(invite => invite.value),
         organizer: ["Moyeen"]
